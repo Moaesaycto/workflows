317c027231349016737d7edcce18852d3fd9ffa6
Moaesaycto <stephen.lerantges.los@gmail.com>
2025-01-30 19:29:05 +1100

    Trying to fix image issue

--- src/utils/file.tsx
+++ src/utils/file.tsx
@@ -1,3 +1,5 @@
+const BASE_URL = import.meta.env.BASE_URL || '';
+
 const allFiles = import.meta.glob('/src/workflows/**/*.md', { as: 'raw' });
 const allAssets = import.meta.glob('/src/workflows/**/style.json', { eager: true, as: 'json' });
 const allIcons = import.meta.glob('/src/workflows/**/icon.png', { eager: true });
@@ -36,7 +38,7 @@ export function getDirectChildFolders(basePath: "workflows", category?: string) 
     Object.entries(allIcons).forEach(([path]) => {
         const parts = path.split('/');
         const subcategory = parts[parts.length - 2];
-        icons[subcategory] = path.replace('/src', '');
+        icons[subcategory] = `${BASE_URL}${path.replace('/src', '')}`;
     });
 
     return {
