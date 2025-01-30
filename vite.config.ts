988e43e22a9d256be1ff45f4be8fae30746c19e4
Moaesaycto <stephen.lerantges.los@gmail.com>
2025-01-30 19:23:09 +1100

    Added md compatibility"

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import mdPlugin from 'vite-plugin-md';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    mdPlugin()
   ],
   base: '/workflows/', // Use your GitHub repository name here
   build: {
